f0f4267887bb5323fc6725c8976774bd14c48508
FernandoM52 <fernandomartins.contato@outlook.com>
2023-04-29 19:10:56 -0300

    fix/eslint config e feat/.editorconfig

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -3,10 +3,10 @@ import { createContext, useState } from "react";
 export const UserContext = createContext();
 
 export default function UserProvider({ children }) {
-    const [user, setUser] = useState({});
-    return (
-        <UserContext.Provider value={{ user, setUser }}>
-            {children}
-        </UserContext.Provider>
-    )
+  const [user, setUser] = useState({});
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  )
 };
