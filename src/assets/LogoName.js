772f16e60b538384912ee167e5b0b80e0e503fb6
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-05-02 12:34:18 -0300

    Logo e Logoname em SVG

---
+++ src/assets/LogoName.js
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+export default function LogoName({ svgHeight, svgWidth }) {
+    return (
+        <Container>
+            <svg width={svgWidth} height={svgHeight} viewBox="0 0 267 34" style={{ backgroundColor: 'transparent' }}>
+                <path id="line" fill="transparent" d="M 0 31 l 310 0" />
+                <text x="0" style={{ fontSize: '40', fontFamily: 'Playfair Display', fontWeight: '800',letterSpacing: '2.4px' }}>
+                    <textPath href="#line">NINE <tspan fill="cadetblue">STORE</tspan></textPath>
+                </text>
+            </svg>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    * {
+        user-select: none;
+    }
+    
+    & svg {
+        display: block;
+    }
+`
